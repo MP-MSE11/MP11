d48127872b27f41dfda59f4963eb9599d0c9ea68
hskim <a4427777@naver.com>
2015-10-19 22:16:04 +0900

    added ImageButton in singleplay Activity

--- MP_test1/app/src/main/java/com/example/hskim7341/mp_test1/Singleplay.java
+++ MP_test1/app/src/main/java/com/example/hskim7341/mp_test1/Singleplay.java
@@ -1,16 +1,30 @@
 package com.example.hskim7341.mp_test1;
 
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
 
 public class Singleplay extends AppCompatActivity {
+
+    ImageButton block1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_singleplay);
+
+        block1 = (ImageButton)findViewById(R.id.block1);
+        block1.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+            }
+        });
     }
 
     @Override
